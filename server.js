9f1fa6a19761834db8c66a08653a0997052a0804
MichalArmon <armonmic22003@gmail.com>
2026-03-25 11:51:56 +0200

    c

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ import uploadsRoutes from "./cloudinary/routes/uploadsRoutes.js";
 import roomRouter from "./rooms/routes/roomsController.js";
 import { connectToDB } from "./DB/dbService.js";
 import treatmentRouter from "./treatments/routes/treatmentsController.js";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
+app.use(cors());
 app.use(express.json());
 
 app.use("/uploads", uploadsRoutes);
